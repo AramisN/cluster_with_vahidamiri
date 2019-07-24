e9f031f1b40c57d03d5bd48c8536845c3578d4ac
vahidamiry <vahid.amiry@gmail.com>
2019-07-24 18:07:11 +0430

    Code Cleaning

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -11,25 +11,15 @@ public class HelloWorld {
 
     public static void main(String[] args) {
 
-
-        System.out.println("Hello World!");
-
-        System.out.println("Hello");
-//        System.out.println(System.getProperty("hadoop.home.dir"));
-
         String inputPath = args[0];
-//        String outputPath = args[1];
-//        String inputPath = "C:\\Users\\amirh\\Desktop\\New Text Document (5).txt";
-
-        SparkConf conf = new SparkConf().setAppName("word-counter").setMaster("local");//.set("spark.cores.max", "10");
+        SparkConf conf = new SparkConf().setAppName("word-counter");//.setMaster("local");
         JavaSparkContext sc = new JavaSparkContext(conf);
         JavaRDD<String> rdd = sc.textFile(inputPath);
         JavaPairRDD<String, Integer> counts = rdd.flatMap(x -> Arrays.asList(x.split(" ")).iterator())
                 .mapToPair(x -> new Tuple2<>(x, 1))
                 .reduceByKey((x, y) -> x + y);
 
-        List<Tuple2<String, Integer>> finalCounts = counts//.filter((x) -> x._1().contains("@"))
-                .collect();
+        List<Tuple2<String, Integer>> finalCounts = counts.collect();
 
         for(Tuple2<String, Integer> count: finalCounts)
             System.out.println(count);
